83412df793b80d9fb782a99bf2481e3fa775657c
Ashley King <Princessak8918@hotmail.com>
2023-05-11 18:19:21 -0600

    pass test

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -1,20 +1,30 @@
+from statistics import mode
 class NationalPark:
-
+ 
     def __init__(self, name):
-        self.name = name
+        if isinstance(name, str)and name:
+            self._name = name
+        else:
+            raise Exception
         self._trips = []
         self._visitors = []
         
-    def trips(self, new_trip=None):
+    @property
+    def name(self):
+            return self._name
+
+        
+    def trips(self):
         from classes.trip import Trip
-        pass
+        return [t for t in Trip.all if t.national_park == self]
     
-    def visitors(self, new_visitor=None):
-        from classes.visitor import Visitor
-        pass
+    def visitors(self):
+        return list(set([t.visitor for t in self.trips() if t.national_park == self]))
+    
     
     def total_visits(self):
-        pass
+        return len(self.trips())
     
     def best_visitor(self):
-        pass
+        visitors = [t.visitor for t in self.trips()]
+        return max(visitors, key=visitors.count)
